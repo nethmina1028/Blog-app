9fecac03811db44c678db3fa72bc4bc61577abd1
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-07-06 11:38:27 +0530

    blog items

--- next-blog-app/src/app/Components/Header.jsx
+++ next-blog-app/src/app/Components/Header.jsx
@@ -13,6 +13,7 @@ function Header() {
     <div className="text-center my-8">
         <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
         <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quo soluta molestiae consectetur eaque, sequi, tempora obcaecati dolore aliquam qui voluptas, voluptatem quibusdam numquam et in accusantium autem nulla commodi.</p>
+        
         <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action=''>
             <input type='email' placeholder='Enter Your Email' className='pl-4 outline-none w-full' />
             <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white ' >Subcribe</button>
